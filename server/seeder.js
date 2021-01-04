83bfb55f22684a069cddbc7354f75454e620f57c
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-01-04 09:46:46 -0600

    created dummy users and a seeder file

---
+++ server/seeder.js
@@ -0,0 +1,40 @@
+const connectDB = require('./config/db')
+const dotenv = require('dotenv')
+dotenv.config()
+
+
+const users = require('./data/users')
+const User = require('./models/userModel')
+
+
+connectDB()
+
+const importData = async () => {
+    try {
+        await User.deleteMany()
+
+        await User.insertMany(users)
+
+        console.log('Data Imported!')
+        process.exit()
+        
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+const destroyData = async () => {
+    try {
+        await User.deleteMany()
+
+        console.log('Data Destroyed!')
+        process.exit()
+        
+    } catch (error) {
+        console.error(error)
+        process.exit(1)
+    }
+}
+
+process.argv[2] === 'd' ? destroyData() : importData()
