43f9f6c56b119c377cf37374ac6017d0ced380ef
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-01-19 14:08:06 -0600

    refactored generateToken.js to use cookie

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,8 +1,17 @@
 const jwt = require('jsonwebtoken')
 
-const generateToken = id => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
+// from this guide:
+// https://dev.to/mr_cea/remaining-stateless-jwt-cookies-in-node-js-3lle
+
+const generateToken = (res, id, firstname) => {
+    const expiration = process.env.DB_ENV === 'testing' ? 100 : 604_800_000
+    const token = jwt.sign({ id, firstName }, process.env.JWT_SECRET, {
+        expiresIn: process.env.DB_ENV === 'testing' ? '1d' : '7d'
+    })
+    return res.cookie('token', token, {
+        expires: new Date(Date.now() + expiration),
+        secure: false, // set to true if using https
+        httpOnly: true
     })
 }
 
