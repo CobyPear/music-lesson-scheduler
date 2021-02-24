8e67efbe348af1cb74ed4a2a30450a032e848c2c
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-23 18:28:36 -0600

    got rid of get * route

--- server/app.js
+++ server/app.js
@@ -53,9 +53,6 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 // Static routes depending on production or development environment
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
-
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
-
 } else {
     // use Morgan logger if in development environment
     app.use(logger('dev'))
