843e72c7afd85a30a32be213596e78a215eab1ed
_Pear <63015754+CobyPear@users.noreply.github.com>
2021-02-23 18:05:24 -0600

    put proper files like package.json and .env  in root directory, not /server

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(session({ 
-    secret: 'lowpass[filter',
+    secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
     store: new MemoryStore({
